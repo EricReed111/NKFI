09f38160a56ada23d94bc68141fd94808941bf79
EricReed111 <148262311+EricReed111@users.noreply.github.com>
2023-10-17 12:59:35 -0400

    Add files via upload

---
+++ NFKI/NFKI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NFKI
+//
+//  Created by Eric Reed on 10/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NFKI/NFKI/NFKIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFKIApp.swift
+//  NFKI
+//
+//  Created by Eric Reed on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct NFKIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
